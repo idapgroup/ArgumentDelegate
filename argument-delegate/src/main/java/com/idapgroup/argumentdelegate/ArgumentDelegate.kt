059b6cbcc241a92fc31f965bd18ce615515ec21d
Jeketos <eugene.kotsogub@idapgroup.com>
2019-06-06 15:24:32 +0300

    Update realisation of argumentDelegate with lazy.

--- argument-delegate/src/main/java/com/idapgroup/argumentdelegate/ArgumentDelegate.kt
+++ argument-delegate/src/main/java/com/idapgroup/argumentdelegate/ArgumentDelegate.kt
@@ -1,49 +1,28 @@
 package com.idapgroup.argumentdelegate
 
 import android.os.Bundle
-import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 /**
  * Function that simplify android.os.Bundle unpacking.
- * @param argumentWrapper block function, that expect android.os.Bundle from object,
+ * @param provideArguments block function, that expect android.os.Bundle from object,
  * Usage example:
  * private val wrapper = { f: Fragment -> a.arguments }
  * @return ReadOnlyProperty, Usage example:
  * val argument: Int by argumentsDelegate(wrapper)
  */
 inline fun <F, reified T> argumentDelegate(
-    crossinline argumentWrapper: (F) -> Bundle
-): ReadOnlyProperty<F, T> =
-    object : ReadOnlyProperty<F, T> {
+    crossinline provideArguments: (F) -> Bundle
+): LazyProvider<F, T> =
+    object : LazyProvider<F, T> {
 
-        private var initialized = false
-        private var value: Any? = null
-
-        override operator fun getValue(thisRef: F, property: KProperty<*>): T {
-            if (!initialized) {
-                val bundle = argumentWrapper(thisRef)
-                value = getArgument<T>(bundle, property)
-                initialized = true
+        override fun provideDelegate(thisRef: F, prop: KProperty<*>) =
+            lazy {
+                val bundle = provideArguments(thisRef)
+                bundle[prop.name] as T
             }
-            return value as T
-        }
     }
 
-inline fun <reified T> getArgument(
-    args: Bundle,
-    property: KProperty<*>
-): T =
-    when (val value = args.get(property.name)) {
-        null -> {
-            if (isNullable<T>()) {
-                null as T
-            } else {
-                throw KotlinNullPointerException("${T::class.java} can't be cast to nullable type")
-            }
-        }
-        is T -> value
-        else -> throw RuntimeException("Property ${property.name} has a wrong type or can't be mapped")
-    }
-
-
+interface LazyProvider<A, T> {
+    operator fun provideDelegate(thisRef: A, prop: KProperty<*>): Lazy<T>
+}
